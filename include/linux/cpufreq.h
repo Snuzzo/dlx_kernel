670cba2d849ab8b79bfcf2c565ef59ec4b51d334
Snuzzo <jrracinfan@gmail.com>
2013-04-11 23:52:38 -0400

    Add Hyper

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -327,6 +327,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_hyper;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hyper)
 #endif
 
 
